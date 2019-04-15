4c4c536789e64fec99706edb915469145e14b09b
Calvin Bruch <calvinbcoding@gmail.com>
2019-04-15 09:02:58 -0600

    Commit 2 - Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ app.use(bodyParser.json());
 //db
 
 
-// const pokemon = require('./pokemon');
+const pokemon = require('./models/pokemon.js');
 
 //new route
-// app.get('/', );
+ app.get('/pokemon', (req,res)=>{
+     res.send(pokemon)
+ } );
 
 app.listen(3000, function() {
     console.log('app listening on port: ', 3000);
