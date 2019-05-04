9efc2a69ea4e6151c0e7cea0f95a51be43b7a854
Calvin Bruch <calvinbcoding@gmail.com>
2019-05-04 09:20:42 -0600

    "added db, new server.js file?"
    Signed-off-by: Calvin Bruch <calvinbcoding@gmail.com>

--- server.js
+++ server.js
@@ -2,24 +2,89 @@ const express  = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const morgan = require('morgan')
 
+// const ejsLint = require('ejs-lint');
 
 //middleware
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(morgan('short'));
+//app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static('public'));
 app.use(bodyParser.json());
-
+app.use(methodOverride('_method'));
 
 //db
+require('./db/db');
+
+const pokemon = require('./pokemon');
+
+// //new route
+//  app.get('/pokemon', (req,res)=>{
+//      console.log('sending pokemon to browser')
+//      res.render('create.ejs')
+//  } );
 
 
-const pokemon = require('./models/pokemon.js');
+ //index
+ app.get('/pokemon', (req, res) => {
+    //res.send(pokemon);
+    console.log('index route');
+     res.render('index.ejs', {
+       pokemon});
+  });
 
-//new route
- app.get('/pokemon', (req,res)=>{
-     res.send(pokemon)
- } );
+  //new
+  app.get('/pokemon/new', (req, res) => {
+    res.render('new.ejs'), {pokemon};
+  });
+
+
+  //create
+  app.post('/pokemon', (req, res) => {
+    pokemon.push(req.body);
+    res.redirect('/pokemon');
+  });
+
+//edit
+  app.get('/pokemon/:id/edit', (req, res) => {
+       res.render('edit.ejs', {
+         pokemon: pokemon[req.params.id]
+       });
+       console.log('edit route');
+  
+    });
+
+    //show
+    app.get('/pokemon/:id', (req, res) => {
+      res.render('show.ejs', {
+        pokemon: pokemon[req.params.id]
+      });
+    });
+
+    //update
+    app.put('/pokemon/:id', (req, res) => {
+      pokemon[req.params.id] = req.body;
+      res.redirect('/pokemon');
+    });
+
+    //delete
+    app.delete('/pokemon/:id', (req, res) => {
+      pokemon.splice(req.params.id, 1);
+      res.redirect('/pokemon');
+    });
+
+//  app.get('show.ejs', (req, res) => {
+//      res.send('index.ejs');
+//      })
+ 
+       //create route
+// app.get('/pokemon', (req, res) => {
+//     res.render('index.ejs')
+//     })
+
 
 app.listen(3000, function() {
     console.log('app listening on port: ', 3000);
 });
 
+module.exports = app;
