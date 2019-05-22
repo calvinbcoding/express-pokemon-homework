c2f1520a98180dc7cd0a944fd2d7a1a8813b41e3
Calvin Bruch <calvinbcoding@gmail.com>
2019-05-22 15:18:28 -0600

    all finished

--- server.js
+++ server.js
@@ -2,49 +2,39 @@ const express  = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const morgan = require('morgan')
+const morgan = require('morgan');
 
-// const ejsLint = require('ejs-lint');
 
 //middleware
 app.use(morgan('short'));
-//app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 
-//db
-require('./db/db');
-
-//const Pokemon = require('./db/db');
-
-// //new route
-//  app.get('/pokemon', (req,res)=>{
-//      console.log('sending pokemon to browser')
-//      res.render('create.ejs')
-//  } );
 
 
- //index
+const Pokemon = require('./db/db');
+
+//index
  app.get('/pokemon', (req, res) => {
-    //res.send(pokemon);
-    console.log('index route');
+    //res.send(Pokemon);
+   console.log('index route<----index');
      res.render('index.ejs', {
        pokemon: Pokemon,
       });
   });
 
-  //new
+  //new-create
   app.get('/pokemon/new', (req, res) => {
+    console.log('new.ejs<--- new.ejs route')
     res.render('new.ejs');
   });
 
-
-  //create
-  app.post('/pokemon', (req, res) => {
-    Pokemon.push(req.body);
-    res.redirect('/pokemon');
-  });
+app.post('/pokemon', (req, res) => {
+  Pokemon.push(req.body);
+  res.redirect('/pokemon');
+}); 
+  
 
 //edit
 app.get('/pokemon/:id/edit', (req, res) => {
@@ -52,7 +42,6 @@ app.get('/pokemon/:id/edit', (req, res) => {
          pokemon: Pokemon[req.params.id],
          id: req.params.id
     });
-  //console.log('edit route');
 });
 
     //show
@@ -75,14 +64,7 @@ app.get('/pokemon/:id/edit', (req, res) => {
       res.redirect('/pokemon');
     });
 
-//  app.get('show.ejs', (req, res) => {
-//      res.send('index.ejs');
-//      })
- 
-       //create route
-// app.get('/pokemon', (req, res) => {
-//     res.render('index.ejs')
-//     })
+
 
 
 app.listen(3000, function() {
---
+++ test.js
@@ -0,0 +1,23 @@
+console.log("ok");
+//db
+//require('./db/db');
+
+
+
+//  app.get('show.ejs', (req, res) => {
+//      res.send('index.ejs');
+//      });
+
+   
+//        //create route
+// app.get('/pokemon', (req, res) => {
+//     res.render('index.ejs')
+//     })
+
+// //create
+
+// // //new route
+//  app.get('/pokemon', (req,res)=>{
+//      console.log('sending pokemon to browser')
+//      res.render('create.ejs')
+//  });
